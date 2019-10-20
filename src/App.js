cd12c0315e5cf3ce5aaf5ffaec2351992b5b9301
Matheus Alves <matheus.alves@meliuz.com.br>
2019-10-20 00:19:45 -0400

    update: aplica exmplo de Section

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import Section from './Section';
+
 function App() {
   return (
     <div className="App">
@@ -19,6 +21,9 @@ function App() {
           Learn React
         </a>
       </header>
+      <Section color="#7FFFD4">
+        Olá Mundo !
+      </Section>
     </div>
   );
 }
